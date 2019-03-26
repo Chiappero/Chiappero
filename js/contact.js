5a0e3a348fa49d6be6d397107f9b1c7da79bfa55
Chiappero <fabien.chiappero@gmail.com>
2019-03-26 22:05:31 +0100

    contact recaptcha and gmaps

--- js/contact.js
+++ js/contact.js
@@ -7,7 +7,31 @@ $(document).ready(function(){
     jQuery.validator.addMethod('answercheck', function (value, element) {
         return this.optional(element) || /^\bcat\b$/.test(value)
     }, "type the correct answer -_-");
+        
+        
+        /**** TODO : recaptcha code to transcript in js
+         if(isset($_POST['g-recaptcha-response'])){
+        $captcha=$_POST['g-recaptcha-response'];
+    }
+    else
+        $captcha = false;
 
+    if(!$captcha){
+        //Do something with error
+    }
+    else{
+        $secret = 'Your secret key here';
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=
+            .$secret.&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+        if($response.success==false)
+        {
+            //Do something with error
+        }
+    }
+... The Captcha is valid you can continue with the rest of your code
+
+
+**/
     // validate contactForm form
     $(function() {
         $('#contactForm').validate({
